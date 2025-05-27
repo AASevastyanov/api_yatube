d0817554a9fd79c50d1fd3ae7eeb7d23bf7e7bf9
AASevastyanov <144042421+AASevastyanov@users.noreply.github.com>
2025-05-27 23:04:17 +0300

    Update permissions.py add enter

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
+
 class IsAuthorOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         if not request.user.is_authenticated:
