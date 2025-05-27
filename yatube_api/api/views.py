48813246c64fe7018c0e622dc2a2d950093b23a6
Aleksey Sevastyanov <sevastyanoff.aloshade@yandex.ru>
2025-05-27 22:46:20 +0300

    flake8

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -3,6 +3,7 @@ from posts.models import Post, Group, Comment
 from .serializers import PostSerializer, GroupSerializer, CommentSerializer
 from .permissions import IsAuthorOrReadOnly
 
+
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.select_related('author', 'group')
     serializer_class = PostSerializer
@@ -11,16 +12,21 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (IsAuthorOrReadOnly,)
 
     def get_queryset(self):
-        return Comment.objects.filter(post_id=self.kwargs['post_id']).select_related('author')
+        return Comment.objects.filter(
+            post_id=self.kwargs['post_id']).select_related('author')
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user, post_id=self.kwargs['post_id'])
+        serializer.save(
+            author=self.request.user,
+            post_id=self.kwargs['post_id'])
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
@@ -11,11 +12,20 @@ class Group(models.Model):
     def __str__(self):
         return self.title
 
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    group = models.ForeignKey(Group, on_delete=models.SET_NULL, related_name='posts', blank=True, null=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts')
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        blank=True,
+        null=True)
     image = models.ImageField(upload_to='posts/', blank=True, null=True)
 
     class Meta:
@@ -24,9 +34,16 @@ class Post(models.Model):
     def __str__(self):
         return self.text[:15]
 
+
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments')
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
